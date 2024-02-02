ed0dc8ba80234d70513e31b09ab270e32c3304b3
Tapesh Dasila <111082667+dasilatapesh@users.noreply.github.com>
2024-02-02 23:30:12 +0530

    added static pages and express extract style and script

--- index.js
+++ index.js
@@ -6,7 +6,13 @@ const port = process.env.PORT || 8000 ;
 
 const expressLayouts = require('express-ejs-layouts');
 
+app.use(express.static('./assets'));
+
 app.use(expressLayouts);
+
+// EXTRACT STYLE AND SCRIPT FROM SUBPAGES INTO THE LAYOUT
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
 
 //USE EXPRESS ROUTER
 app.use('/', require('./routes')); //automatically fetches index.js
